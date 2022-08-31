f5b802c9515048d55830a3828fd40c8ed88408bb
willik28 <willik28@gmail.com>
2022-08-30 20:52:25 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,17 @@
+myfloat =  7.0
+print(myfloat)
+myint = 90
+print(myint)
+mystring = "testing variables"
+print(mystring)
+my_string = 'holy cow batman'
+print(my_string)
+
+number = myfloat + myint
+
+print(number)
+
+print(my_string + ", " + mystring)
+
+a, b = 3, 4
+print(a, b)
